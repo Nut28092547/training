a614d0c835e2185d461b385d313db6ef37e798f2
Nut28092547 <beein00998a@gmail.com>
2024-09-07 15:48:42 +0700

    commit-message

---
+++ src/pages/Course.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout';
+
+const Course = () => {
+  return (
+    <Layout>
+      <h1 className="font-bold text-4xl text-blue-950 mb-2 text-center">หลักสูตรที่เปิดสอน</h1>
+      <hr />
+
+      <p className="text-xl text-center mt-5"></p>
+    </Layout>
+  )
+}
+
+export default Course
